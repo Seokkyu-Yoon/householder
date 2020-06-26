4452b4c0b47bef005974c84c04c380d6057ae928
윤석규 <yskyu93@modutech.co.kr>
2020-06-26 17:49:49 +0900

    refactor(Tunnel): refactor to use Runnable

--- app/src/main/java/com/example/householder/MainActivity.java
+++ app/src/main/java/com/example/householder/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.householder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.SocketException;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnSignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignin = (Button) findViewById(R.id.btn_signin);
+        btnSignin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_signin:
+                Toast.makeText(getApplicationContext(), "request connect", Toast.LENGTH_SHORT).show();
+                Tunnel tunnel = new Tunnel("seokkyu");
+                new Thread(tunnel).start();
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/householder/Tunnel.java
+++ app/src/main/java/com/example/householder/Tunnel.java
@@ -9,27 +9,47 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.SocketException;
 
-public class Tunnel {
-    private static final String ip = "http://yeochan.iptime.org";
-    private static final int port = 8080;
-    private static final String user = "db";
-    private static final String password = "db1127";
+public class Tunnel implements Runnable{
+    private static final String IP = "http://yeochan.iptime.org";
+    private static final int PORT = 8080;
+    private static final String DB_USER = "db";
+    private static final String DB_PASSWORD = "db1127";
+    private static final int BEFORE_LOGIN = 0;
 
-    private FTPClient connect() throws SocketException, IOException {
+    private int state = 0;
+    private String id;
+    public Tunnel(String id) {
+        this.id = id;
+    }
+
+    private FTPClient connect() {
         FTPClient ftpClient = new FTPClient();
         try {
-            ftpClient.connect(this.ip, this.port);
-            ftpClient.login(this.user, this.password);
+            ftpClient.connect(IP, PORT);
+            ftpClient.login(DB_USER, DB_PASSWORD);
         } finally {
             return ftpClient;
         }
     }
 
-    public void upload(File file, String filename) throws SocketException, IOException, Exception {
+    public void upload(File file, String filename) {
         try {
             FTPClient ftpClient = connect();
         } finally {
 
+        }
+    }
+
+    @Override
+    public void run() {
+        if (state == 0) {
+            FTPClient ftpClient = connect();
+            try {
+                int i = ftpClient.pwd();
+                System.out.println(i);
+            } catch (IOException e) {
+                System.out.println("**************************** error ****************************");
+            }
         }
     }
 }
